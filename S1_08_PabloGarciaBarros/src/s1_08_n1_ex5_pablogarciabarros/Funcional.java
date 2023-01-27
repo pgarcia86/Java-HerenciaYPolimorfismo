a6479bc27de71a2cb07fc0291727eaa31358d288
pgarcia86 <pgarciabarros86@gmail.com>
2023-01-27 17:29:58 +0100

    Delete S1_08_PabloGarciaBarros directory

--- S1_08_PabloGarciaBarros/src/s1_08_n1_ex5_pablogarciabarros/Funcional.java
+++
@@ -1,7 +0,0 @@
-package s1_08_n1_ex5_pablogarciabarros;
-
-@FunctionalInterface
-interface Funcional {
-
-	public double getValorPi();
-}
--- S1_08_PabloGarciaBarros/src/s1_08_n2_ex3_pablogarciabarros/Funcional.java
+++
@@ -1,8 +0,0 @@
-package s1_08_n2_ex3_pablogarciabarros;
-
-@FunctionalInterface
-public interface Funcional {
-	
-	public float operacion(float num1, float num2);
-
-}
